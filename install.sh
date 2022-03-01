c8b528a3ed14580e80056ed506ad4ff89fd462c6
Khronos31 <39792509+Khronos31@users.noreply.github.com>
2022-03-01 16:23:33 +0900

    とりあえずinstal.shを作った

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# Khronos31 | install.sh
+#
+
+timestamp="$(date +%Y%m%d_%H%M%S)"
+
+dotfiles=(.bash_profile .bashrc .zshrc .commonrc .common_aliases .gitconfig)
+
+for file in "${dotfiles[@]}"; do
+  if [ -e "$HOME/$file" ]; then
+    mv "$HOME/$file" "$HOME/$file-$timestamp.old)"
+  fi
+  cp "$file" "$HOME/$file"
+  chmod 644 "$HOME/$file"
+done
